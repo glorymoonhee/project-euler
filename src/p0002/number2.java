3650e1d25706703ff19372db7ff27e183bb10d22
g6g6g63216 <g6g6g63216@naver.com>
2015-08-03 18:21:27 +0900

    두번째 문제 풀었음

--- src/p0002/number2.java
+++ src/p0002/number2.java
@@ -2,7 +2,33 @@ package p0002;
 
 public class number2 {
 
+	public static void main(String[] args) {
 	
+		long result = getFibo(); 
+		System.out.println(result);
+	}
+
+	private static long getFibo() {
+		 
+	   
+	     long n_2 = 1;   //첫번째 값을 설정
+	     long n_1 = 2; 
+	     long temp = 0;
+	     long sum =0;
+	     long max = 4000000;
+
+        while(n_2<max){
+        	    temp= n_2 + n_1;
+                n_2 = n_1;
+                n_1 = temp;
+        	   
+                if(n_2%2==0){
+                	sum += n_2;
+                }
+        }
+	     
+	      return sum;
+	         
+	}
 	
-/// 	dddddddddd
 }
